9105956a370d120791fccc479104c039e730df4a
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-05-06 08:49:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import math
-rad = float(input("Enter the radius of the circle :"))
+rad = float(input("Input the radius of the circle :"))
 area= math.pi * rad * rad
-print("Area of the circle is :{0}".format(area))
+print("The area of the circle with the radius {0} ".format(rad))
+print("is {0}".format(area))
